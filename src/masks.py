f74fe810744d8ae9ee91d832ccae84f039c0231f
Ольга Черняга <ochernyaga@gmail.com>
2026-08-05 23:13:58 +0300

    init

---
+++ src/masks.py
@@ -0,0 +1,24 @@
+def get_mask_card_number(card_number: int) -> "str | int":
+    """
+    Функция маскировки номера банковской карты
+    если номер = 16 символов возвращает маску
+    если нет - возвращает номер
+    """
+    card_number_str = str(card_number)
+    if len(card_number_str) == 16:
+        return card_number_str[:4] + " " + card_number_str[5:7] + "** **** " + card_number_str[-4:]
+    else:
+        return card_number
+
+
+def get_mask_account(account_number: int) -> "str | int":
+    """
+    Функция маскировки номера банковского счета
+    если номер ≥4 символов возвращает маску
+    если нет - возвращает номер
+    """
+    account_number_str = str(account_number)
+    if len(account_number_str) >= 4:
+        return "**" + account_number_str[-4:]
+    else:
+        return account_number
