04dd82c89ed6dbfc0c039e69b9f0552fae1ef951
Ольга Черняга <ochernyaga@gmail.com>
2026-08-10 01:19:21 +0300

    after checks DZ_10.1

--- src/processing.py
+++ src/processing.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 
 
-def filter_by_state(processes: list[dict], state: str='EXECUTED') -> list[dict]:
+def filter_by_state(processes: list[dict], state: str = "EXECUTED") -> list[dict]:
     """
     Принимает список словарей и опционально значение для ключа state
      (по умолчанию 'EXECUTED').
@@ -10,17 +10,17 @@ def filter_by_state(processes: list[dict], state: str='EXECUTED') -> list[dict]:
     """
     result = []
     for process in processes:
-        if process['state'] == state:
+        if process["state"] == state:
             result.append(process)
 
     return result
 
 
-def sort_by_date(processes: list[dict], reverse: bool=True) -> list[dict]:
+def sort_by_date(processes: list[dict], reverse: bool = True) -> list[dict]:
     """
     Принимает список словарей и необязательный параметр reverse, задающий порядок сортировки.
     При reverse = True сортирует по убыванию (по умолчанию),
     при reverse = False - сортировка по возрастанию.
     Функция возвращает новый список, отсортированный по дате (date).
     """
-    return sorted(processes,key = lambda x: datetime.fromisoformat(x['date']), reverse=reverse)
+    return sorted(processes, key=lambda x: datetime.fromisoformat(x["date"]), reverse=reverse)
