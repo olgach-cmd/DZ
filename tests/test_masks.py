bfda151a14bbe2597bb52c549b29ab5e93a0a27c
Ольга Черняга <ochernyaga@gmail.com>
2026-08-16 02:21:20 +0300

    add htmlcov/ and formatting tests/

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -1,20 +1,26 @@
 import pytest
 
-from src.masks import get_mask_card_number, get_mask_account
+from src.masks import get_mask_account, get_mask_card_number
 
 
 @pytest.fixture
 def number_not_digit():
     return "dlfkgjlsdkskfsdl"
+
+
 @pytest.fixture
 def few_number():
     return "78"
 
-@pytest.mark.parametrize("card_number, expected", [
-    ("7000792289606361", "7000 79** **** 6361"),
-    ("1596837868705199", "1596 83** **** 5199"),
-    ("7158300734726758", "7158 30** **** 6758"),
-])
+
+@pytest.mark.parametrize(
+    "card_number, expected",
+    [
+        ("7000792289606361", "7000 79** **** 6361"),
+        ("1596837868705199", "1596 83** **** 5199"),
+        ("7158300734726758", "7158 30** **** 6758"),
+    ],
+)
 def test_get_mask_card_number(card_number, expected):
     assert get_mask_card_number(card_number) == expected
 
@@ -29,12 +35,15 @@ def test_get_mask_card_number_len(few_number):
         get_mask_card_number(few_number)
 
 
-@pytest.mark.parametrize("account_number, expected", [
-    ("64686473678894779589", "**9589"),
-    ("35383033474447895560", "**5560"),
-    ("6831982476737658", "**7658"),
-    ("8247", "**8247"),
-])
+@pytest.mark.parametrize(
+    "account_number, expected",
+    [
+        ("64686473678894779589", "**9589"),
+        ("35383033474447895560", "**5560"),
+        ("6831982476737658", "**7658"),
+        ("8247", "**8247"),
+    ],
+)
 def test_get_mask_account(account_number, expected):
     assert get_mask_account(account_number) == expected
 
--- tests/test_widget.py
+++ tests/test_widget.py
@@ -1,55 +1,68 @@
 import pytest
 
-from src.widget import mask_account_card, get_date
+from src.widget import get_date, mask_account_card
 
 
-@pytest.mark.parametrize("num_account_card, expected", [
-    ("Maestro 1596837868705199", "Maestro 1596 83** **** 5199"),
-    ("Счет 64686473678894779589", "Счет **9589"),
-    ("MasterCard 7158300734726758", "MasterCard 7158 30** **** 6758"),
-    ("Счет 35383033474447895560", "Счет **5560"),
-    ("Visa Classic 6831982476737658", "Visa Classic 6831 98** **** 7658"),
-    ("Visa Platinum 8990922113665229", "Visa Platinum 8990 92** **** 5229"),
-    ("Visa Gold 5999414228426353", "Visa Gold 5999 41** **** 6353"),
-    ("Счет 73654108430135874305", "Счет **4305"),
-])
+@pytest.mark.parametrize(
+    "num_account_card, expected",
+    [
+        ("Maestro 1596837868705199", "Maestro 1596 83** **** 5199"),
+        ("Счет 64686473678894779589", "Счет **9589"),
+        ("MasterCard 7158300734726758", "MasterCard 7158 30** **** 6758"),
+        ("Счет 35383033474447895560", "Счет **5560"),
+        ("Visa Classic 6831982476737658", "Visa Classic 6831 98** **** 7658"),
+        ("Visa Platinum 8990922113665229", "Visa Platinum 8990 92** **** 5229"),
+        ("Visa Gold 5999414228426353", "Visa Gold 5999 41** **** 6353"),
+        ("Счет 73654108430135874305", "Счет **4305"),
+    ],
+)
 def test_mask_account_card(num_account_card, expected):
     assert mask_account_card(num_account_card) == expected
 
-@pytest.mark.parametrize("err_number", [
-    ("Maestro 15968378199"),
-    ("Счет 89"),
-    ("MasterCard krjdngkcmdjtkglf"),
-    ("krjdngkcmdjtkglf"),
-    ("5999414228426353")
-])
+
+@pytest.mark.parametrize(
+    "err_number",
+    [
+        ("Maestro 15968378199"),
+        ("Счет 89"),
+        ("MasterCard krjdngkcmdjtkglf"),
+        ("krjdngkcmdjtkglf"),
+        ("5999414228426353"),
+    ],
+)
 def test_mask_account_card_err(err_number):
     with pytest.raises(ValueError) as exc_info:
         mask_account_card(err_number)
+
 
 # @pytest.fixture
 # def date():
 #     return "2019-07-03T18:35:29.512364"
 
 
-@pytest.mark.parametrize("date, expected", [
-    ("2019-07-03T18:35:29.512364", "03.07.2019"),
-    ("2018-06-30T02:08:58.425572", "30.06.2018"),
-    ("2018-09-12T21:27:25.241689", "12.09.2018"),
-    ("2018-10-14T08:21:33.419441", "14.10.2018"),
-    ("2024-03-11T02:26:18.671407", "11.03.2024"),
-])
-
+@pytest.mark.parametrize(
+    "date, expected",
+    [
+        ("2019-07-03T18:35:29.512364", "03.07.2019"),
+        ("2018-06-30T02:08:58.425572", "30.06.2018"),
+        ("2018-09-12T21:27:25.241689", "12.09.2018"),
+        ("2018-10-14T08:21:33.419441", "14.10.2018"),
+        ("2024-03-11T02:26:18.671407", "11.03.2024"),
+    ],
+)
 def test_get_date(date, expected):
     assert get_date(date) == expected
+
 
 @pytest.fixture
 def incorrect_date_format():
     return "2024-03-56T02:26:18.671407"
 
+
 def test_get_date_incorrect_format(incorrect_date_format):
     with pytest.raises(ValueError) as exc_info:
         get_date(incorrect_date_format)
+
 
 def test_get_date_no_date():
     with pytest.raises(ValueError) as exc_info:
