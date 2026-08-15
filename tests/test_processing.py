76e4959ded37f90ab3f74084933a2d988c1f10ea
Ольга Черняга <ochernyaga@gmail.com>
2026-08-16 01:55:58 +0300

    add test_processing.py

---
+++ tests/test_processing.py
@@ -0,0 +1,74 @@
+import pytest
+
+from src.processing import filter_by_state, sort_by_date
+
+
+@pytest.fixture
+def processes():
+    return [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+            {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+            {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+            {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
+
+@pytest.fixture
+def expected_no_state_for_filter_by_state():
+    return [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+            {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}]
+
+@pytest.fixture
+def expected_no_reverse_for_sort_by_date():
+    return [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+            {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
+            {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+            {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}]
+
+
+
+@pytest.mark.parametrize("state, expected", [
+    ("EXECUTED", [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+                  {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}]),
+    ("CANCELED", [{'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+                  {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]),
+    ("PROCESS", []),
+])
+
+
+def test_filter_by_state(processes, state, expected):
+    assert filter_by_state(processes, state) == expected
+
+
+def test_filter_by_state_no_state(processes, expected_no_state_for_filter_by_state):
+    assert filter_by_state(processes) == expected_no_state_for_filter_by_state
+
+
+@pytest.mark.parametrize("reverse, expected", [
+    (True, [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+            {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
+            {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+            {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}]),
+    (False, [{'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+             {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+             {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
+             {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}]),
+])
+def test_sort_by_date(processes, reverse, expected):
+    assert sort_by_date(processes, reverse) == expected
+
+
+def test_sort_by_date_no_reverse(processes, expected_no_reverse_for_sort_by_date):
+    assert sort_by_date(processes) == expected_no_reverse_for_sort_by_date
+
+@pytest.mark.parametrize("processes, expected", [
+    ([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+      {'id': 615064591, 'state': 'CANCELED', 'date': '2019-07-03'},
+      {'id': 594226727, 'state': 'CANCELED', 'date': '2019-07-03 18:35:29'},
+      {'id': 939719570, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:30'}],
+
+     [{'id': 939719570, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:30'},
+      {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+      {'id': 594226727, 'state': 'CANCELED', 'date': '2019-07-03 18:35:29'},
+      {'id': 615064591, 'state': 'CANCELED', 'date': '2019-07-03'}]
+     ),
+])
+def test_sort_by_date_same(processes, expected):
+    assert sort_by_date(processes) == expected
