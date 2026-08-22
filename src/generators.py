5126ba321f80e0d25c70dfab19590478e44415df
Ольга Черняга <ochernyaga@gmail.com>
2026-08-22 18:45:46 +0300

    formatting generators.py and test_generators.py, update htmlcov

--- src/generators.py
+++ src/generators.py
@@ -1,4 +1,4 @@
-from typing import Any, Generator
+from typing import Generator
 
 
 def filter_by_currency(transactions: list[dict], code: str) -> Generator[dict, None, None]:
@@ -18,7 +18,8 @@ def transaction_descriptions(transactions: list[dict]) -> Generator[dict, None, 
     и возвращает описание каждой операции по очереди (например, "Перевод организации")
     """
     for transaction in transactions:
-         yield transaction["description"]
+        yield transaction["description"]
+
 
 def card_number_generator(start: int, stop: int) -> Generator[str, None, None]:
     """
@@ -26,12 +27,10 @@ def card_number_generator(start: int, stop: int) -> Generator[str, None, None]:
     Генератор может сгенерировать номера карт в заданном диапазоне от 0000 0000 0000 0001 до 9999 9999 9999 9999.
     """
     if len(str(start)) > 16 or start <= 0 or len(str(stop)) > 16 or stop < start:
-        raise ValueError("Неверно задан диапазон значений. "
-                         "Сгенерировать номер карт можно в диапазоне от 1 до 9999999999999999")
-    for i in range(start, stop+1):
+        raise ValueError(
+            "Неверно задан диапазон значений. Сгенерировать номер карт можно в диапазоне от 1 до 9999999999999999"
+        )
+    for i in range(start, stop + 1):
         str_i = str(i)
-        num_cart = "0"*(16-len(str_i))+str_i
+        num_cart = "0" * (16 - len(str_i)) + str_i
         yield f"{num_cart[0:4]} {num_cart[4:8]} {num_cart[8:12]} {num_cart[12:16]}"
-
-
-
