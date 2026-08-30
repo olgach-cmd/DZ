c7b1b47ef443ae63b69215628311bc7db9bd9662
Ольга Черняга <ochernyaga@gmail.com>
2026-08-30 20:18:01 +0300

    add tests for list_transactions_from_file

---
+++ tests/test_utils.py
@@ -0,0 +1,15 @@
+import pytest
+from src.utils import list_transactions_from_file
+
+
+# Тесты list_transactions_from_file
+def test_list_transactions_from_file(list_transactions):
+    assert list_transactions_from_file(r"data\operations.json") == list_transactions
+
+
+def test_list_transactions_from_file_clear():
+    assert list_transactions_from_file(r"data\operations_clear.json") == []
+
+
+def test_list_transactions_from_file_no_json():
+    assert list_transactions_from_file(r"data\operations_no_json.json") == []
